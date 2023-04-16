a46568ee42b3cc998be8fa7adee57f6ba5e72360
guest271314 <guest271314@gmail.com>
2023-04-16 16:14:22 -0700

    Use single handler, pass formatter function

--- panel.js
+++ panel.js
@@ -1,18 +1,13 @@
-chrome.devtools.panels.create("Formatter", null, "formatter.html");
-
 onload = () => {
+  const handleFormat = (formatter) => textarea.value = formatter(textarea.value, {
+    indent_size: 2
+  });
   const textarea = document.querySelector('textarea');
   const [js, html, css] = document.querySelectorAll('button');
-  js.onclick = () => textarea.value = js_beautify(textarea.value, {
-    indent_size: 2
-  });
-  html.onclick = () => textarea.value = html_beautify(textarea.value, {
-    indent_size: 2
-  });
-  css.onclick = () => textarea.value = css_beautify(textarea.value, {
-    indent_size: 2
-  });
-  textarea.onpaste = (e) => {
+  js.onclick = () => handleFormat(js_beautify);
+  html.onclick = () => handleFormat(html_beautify);
+  css.onclick = () => handleFormat(css_beautify);
+  textarea.onpaste = async (e) => {
     e.preventDefault();
     // Remove leading space characters
     e.target.value = e.clipboardData.getData('text').replace(/^\s+/, '');
