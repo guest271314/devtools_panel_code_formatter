63201139c6a3c49895485f80ccaa5bb9a7e14113
guest271314 <guest271314@gmail.com>
2023-04-16 16:23:47 -0700

    Pass options to formatter function

--- panel.js
+++ panel.js
@@ -1,13 +1,16 @@
+chrome.devtools.panels.create("Formatter", null, "formatter.html");
+
 onload = () => {
-  const handleFormat = (formatter) => textarea.value = formatter(textarea.value, {
+  const options = {
     indent_size: 2
-  });
+  };
+  const handleFormat = (formatter, options) => textarea.value = formatter(textarea.value, options);
   const textarea = document.querySelector('textarea');
   const [js, html, css] = document.querySelectorAll('button');
-  js.onclick = () => handleFormat(js_beautify);
-  html.onclick = () => handleFormat(html_beautify);
-  css.onclick = () => handleFormat(css_beautify);
-  textarea.onpaste = async (e) => {
+  js.onclick = () => handleFormat(js_beautify, options);
+  html.onclick = () => handleFormat(html_beautify, options);
+  css.onclick = () => handleFormat(css_beautify, options);
+  textarea.onpaste = (e) => {
     e.preventDefault();
     // Remove leading space characters
     e.target.value = e.clipboardData.getData('text').replace(/^\s+/, '');
