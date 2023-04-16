013f3e1c7f38e7e6d4c4d90ecab5552d23d3e27e
guest271314 <guest271314@gmail.com>
2023-04-16 13:37:38 -0700

    Create panel.js

---
+++ panel.js
@@ -0,0 +1,20 @@
+chrome.devtools.panels.create("Formatter", null, "formatter.html");
+
+onload = () => {
+  const textarea = document.querySelector('textarea');
+  const [js, html, css] = document.querySelectorAll('button');
+  js.onclick = () => textarea.value = js_beautify(textarea.value, {
+    indent_size: 2
+  });
+  html.onclick = () => textarea.value = html_beautify(textarea.value, {
+    indent_size: 2
+  });
+  css.onclick = () => textarea.value = css_beautify(textarea.value, {
+    indent_size: 2
+  });
+  textarea.onpaste = async(e) => {
+    e.preventDefault();
+    // Remove leading space characters
+    e.target.value = e.clipboardData.getData('text').replace(/^\s+/, '');
+  }
+}
